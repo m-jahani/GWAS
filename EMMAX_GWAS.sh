6ceb016ec85665ea2edc6d34b76dabf9e7506254
Mojtaba Jahani <moja987@gmail.com>
2022-03-23 13:03:12 -0700

    debug EMMAX_GWAS.sh

--- EMMAX_GWAS.sh
+++ EMMAX_GWAS.sh
@@ -8,5 +8,5 @@ DIRECTORY=${DIR%%\/}
 DIRVCF=${DIRECTORY}/${VCF##*/}
 NAME=${DIRVCF%%.vcf.gz}
 
-/home/mjahani/bin/emmax-intel64 -v -d 10 -t $NAME -p $PHENOTYPE_FILE -k ${NAME}.aIBS.kinf -c ${NAME}.cov -o ${OUTPUT_DIR}${NAME##*/}_$(basename "${PHENOTYPE_FILE%%.txt}")
+/home/mjahani/bin/emmax-intel64 -v -d 10 -t $NAME -p $PHENOTYPE_FILE -k ${NAME}.aIBS.kinf -c ${NAME}.cov -o ${OUTPUT_DIR}/${NAME##*/}_$(basename "${PHENOTYPE_FILE%%.txt}")
 Rscript manhattan.R ${OUTPUT_DIR}${NAME##*/}_$(basename "${PHENOTYPE_FILE%%.txt}").ps $OUTPUT_DIR
